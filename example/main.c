71e1ae3604cb583e963ce27913f8493ffaf87300
Christos Houtouridis <houtouridis.ch@gmail.com>
2014-04-19 23:15:49 +0300

    Example/Test project changes

--- example/main.c
+++ example/main.c
@@ -1,36 +1,71 @@
 #include <toolbox.h>
 
-pid_t pid;
-tne_t tne;
-uint8_t hash [32];
-aes_t key;
 
-uint8_t text [16] =
-{
-   1, 2, 3, 4, 5, 6, 7, 8, 9, 0 ,1, 2, 3, 4, 5, 6
-};
-
-int main(void)
+void acs_test (void)
 {
    int i;
+   pid_c_t pid;
+   tne_t tne;
 
-   //pid_init(&pid, 1, 0.25, 0.15, 0.1, 0.01);
-   //tne_init(&tne, 1, 0.1, 0.05, 0.1);
-   //tne_sat (&tne, 10, -30);
+   pid_init(&pid, 1, 0.25, 0.15, 0.1, 0.01);
+   tne_init(&tne, 1, 0.1, 0.05, 0.1);
+   tne_sat (&tne, 10, -30);
 
-   sha2 ((uint8_t*)"apassword",  9, hash, SHA2_256);
+   for (i=0 ; i<20 ; ++i)
+   {
+      pid_out(&pid, 0.1);
+      tne_out(&tne, 0.06);
+   }
+}
+
+void crypt_test (void)
+{
+   uint8_t hash [32];
+   aes_t key;
+   uint8_t pass[11] = "a-password";
+   uint8_t text [16] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 ,1, 2, 3, 4, 5, 6 };
+
+   sha2 ((uint8_t*)pass,  9, hash, SHA2_256);
    aes_key_init (&key, hash, AES_256);
 
    aes_encrypt (&key, text, text);
    aes_decrypt (&key, text, text);
 
    aes_key_deinit (&key);
+}
 
-   for (i=0 ; i<20 ; ++i)
-   {
-      //pid_out(&pid, 0.1);
-      //tne_out(&tne, 0.06);
-   }
+void dsp_test (void)
+{
+   leaky_int_t li;
+   moving_av_t ma;
+   float out;
+
+   leaky_int_init (&li, 0.85);
+   out = leaky_int (&li, 1);
+   out = leaky_int (&li, 1);
+
+   moving_av_init(&ma, 4);
+   out = moving_av(&ma, 1);
+   out = moving_av(&ma, 1);
+   out = moving_av(&ma, 1);
+
+   out+=1;  // Discard warning
+}
+
+void std_test (void)
+{
+   time_t bd = 0;
+   struct tm *t = gmtime (&bd);
+   
+   bd = smktime (t);
+}
+
+int main(void)
+{
+   acs_test ();
+   crypt_test ();
+   dsp_test ();
+   std_test ();
 
    while(1)
       ;
