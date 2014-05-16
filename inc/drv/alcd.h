01fa4b7bfef4c0005a1c2ea7685154538ef3bbc7
Christos Houtouridis <houtouridis.ch@gmail.com>
2014-05-16 17:24:21 +0300

    alcd: alcd_pin_t pointer fix

--- inc/drv/alcd.h
+++ inc/drv/alcd.h
@@ -106,7 +106,7 @@ typedef volatile struct
    uint8_t     y;
 }alcd_cursor_t;
 
-typedef void (*alcd_pin_t) (int8_t);
+typedef void (*alcd_pin_t) (uint8_t);
 
 /*!
  * Alpharithmetic LCD Pin assignements.
@@ -171,6 +171,7 @@ int alcd_putchar (alcd_t *alcd, int ch);
  */
 void alcd_deinit (alcd_t *alcd);                /*!< For compatibility */
 drv_status_en alcd_init (alcd_t *alcd);         /*!< For compatibility */
+
 void alcd_backlight (alcd_t *alcd, int8_t on);  /*!< For compatibility */
 void alcd_enable (alcd_t *alcd, uint8_t on);    /*!< For compatibility */
 void alcd_cls (alcd_t *alcd);                   /*!< For compatibility */
