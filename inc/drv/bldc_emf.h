9158572c63819ab58d0aaa9dd34f03e1dd7b5533
Christos Houtouridis <houtouridis.ch@gmail.com>
2014-09-05 10:49:21 +0300

    bldc: A little work

--- inc/drv/bldc_emf.h
+++ inc/drv/bldc_emf.h
@@ -1,5 +1,5 @@
 /*!
- * \file bldc3_emf.h
+ * \file bldc_emf.h
  * \brief
  *    A target independent brushless DC motor with back-emf.
  *
@@ -21,8 +21,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
-#ifndef __bldc3_emf_h__
-#define __bldc3_emf_h__
+#ifndef __bldc_emf_h__
+#define __bldc_emf_h__
 
 #ifdef __cplusplus
 extern "C" {
@@ -37,9 +37,9 @@ extern "C" {
 /*
  * ==================  USER defines   =======================
  */
-#define BLDC3_JF_FREQ                     (1000000)   //1MHz - time base 1usec
-#define BLDC3_JF_ARVALUE                  (0x8FFF)
-#define BLDC3_MIN_STEP_PER_COMMUTE        (500)
+#define BLDC_JF_FREQ                      (1000000)   //1MHz - time base 1usec
+#define BLDC_JF_ARVALUE                   (0x8FFF)
+#define BLDC_MIN_STEP_PER_COMMUTE         (500)
 /*!<
  * Indicates the minimum check points per zc period
  */
@@ -48,41 +48,41 @@ extern "C" {
 /*
  * ================== General defines =======================
  */
-#define BLDC3_STATES                   (6)         // 3 phace system
-#define BLDC3_MIN_RPM                  (60)        // 1 Hz
-#define BLDC3_MAX_RPM(_poles)          ( (60*BLDC3_JF_FREQ) / (BLDC3_MIN_STEP_PER_COMMUTE * (_poles)) )
+#define BLDC_STATES                    (6)         // 3 phace system
+#define BLDC_MIN_RPM                   (60)        // 1 Hz
+#define BLDC_MAX_RPM(_poles)           ( (60*BLDC_JF_FREQ) / (BLDC_MIN_STEP_PER_COMMUTE * (_poles)) )
 /*!<
  *             60 * JF_CLK
  *   RPM <= -------------------
  *           MIN_STEP * POLES
  *
- * MIN_STEP = BLDC3_DT_PER_COMMUTE
+ * MIN_STEP = BLDC_DT_PER_COMMUTE
  */
 
-#define BLDC3_RPM2USEC(_rpm, _poles)   ( (60 * 1000000) / ((_rpm)*(_poles)) )
+#define BLDC_RPM2USEC(_rpm, _poles)    ( (60 * 1000000) / ((_rpm)*(_poles)) )
 
-typedef int bldc3_rpm_t;
+typedef int bldc_rpm_t;
 
 
 /*!
  * 3 Phase Brushless DC motor output bridge driving state
  */
 typedef enum {
-   BLDC3_ST0 = 0,    /*!< UL(PWM), VH(ON), W(ZC) */
-   BLDC3_ST1,        /*!< WH(PWM), UL(ON), V(ZC) */
-   BLDC3_ST2,        /*!< VL(PWM), WH(ON), U(ZC) */
-   BLDC3_ST3,        /*!< UH(PWM), VL(ON), W(ZC) */
-   BLDC3_ST4,        /*!< WL(PWM), UH(ON), V(ZC) */
-   BLDC3_ST5         /*!< VH(PWM), WL(ON), U(ZC) */
-}bldc3_state_en;
+   BLDC_ST0 = 0,     /*!< UL(PWM), VH(ON), W(ZC) */
+   BLDC_ST1,         /*!< WH(PWM), UL(ON), V(ZC) */
+   BLDC_ST2,         /*!< VL(PWM), WH(ON), U(ZC) */
+   BLDC_ST3,         /*!< UH(PWM), VL(ON), W(ZC) */
+   BLDC_ST4,         /*!< WL(PWM), UH(ON), V(ZC) */
+   BLDC_ST5          /*!< VH(PWM), WL(ON), U(ZC) */
+}bldc_state_en;
 
 /*!
  * BLDC motor direction enumerator
  */
 typedef enum {
-   BLDC3_REV = -1,      /*!< Reverse */
-   BLDC3_FWD = 1        /*!< Forward */
-}bldc3_dir_en;
+   BLDC_REV = -1,       /*!< Reverse */
+   BLDC_FWD = 1         /*!< Forward */
+}bldc_dir_en;
 
 /*!
  * I/O link function pointers
@@ -96,7 +96,7 @@ typedef struct {
    drv_ain_i_ft   w_emf;      /*!< analog zc function, return integer ADC value */
    drv_ain_f_ft   I_br;       /*!< analog I bridge function, return current value as float */
    pid_c_t        *pid;       /*!< closed loop PID controller */
-}bldc3_link_t;
+}bldc_link_t;
 
 /*!
  * BLDC motor data struct
@@ -106,7 +106,7 @@ typedef struct {
    int   Freq_zc;             /*!< zero cross frequency */
    float I_br;                /*<! Total bridge current */
    float V_br;                /*<! Bridge voltage */
-}bldc3_in_data_t;
+}bldc_in_data_t;
 
 /*!
  * User settings for the control loop
@@ -114,9 +114,9 @@ typedef struct {
 typedef struct {
    int            Freq_r;     /*!< Desired rotation frequency */
    float          I_br;       /*!< Desired Maximum bridge current */
-   bldc3_dir_en   dir;        /*!< Desired direction of rotation */
+   bldc_dir_en   dir;        /*!< Desired direction of rotation */
    int            poles;      /*!< Number of Motor poles */
-}bldc3_set_t;
+}bldc_set_t;
 
 /*!
  * Event struct
@@ -124,61 +124,62 @@ typedef struct {
 typedef struct {
    uint8_t  zc       :1;      /*!< zero cross event */
    uint8_t  commute  :1;      /*!< Commute event */
-}bldc3_event_t;
+}bldc_event_t;
 
 /*!
  * 3 phase BLDC motor type
  */
 typedef volatile struct {
-   bldc3_link_t      io;      /*!< link to application low level driver */
-   bldc3_in_data_t   in;      /*!< Input data */
-   bldc3_set_t       set;     /*!< Settings */
-   bldc3_event_t     event;   /*!< Events */
-   bldc3_state_en    state;   /*!< Current output driving state */
+   bldc_link_t       io;      /*!< link to application low level driver */
+   bldc_in_data_t    in;      /*!< Input data */
+   bldc_set_t        set;     /*!< Settings */
+   bldc_event_t      event;   /*!< Events */
+   bldc_state_en     state;   /*!< Current output driving state */
    drv_status_en     status;  /*!< Driver's status */
-}bldc3_t;
+}bldc_t;
 
 
 /*
- *  ============= PUBLIC BLDC3 API =============
+ *  ============= PUBLIC BLDC API =============
  */
 
 /*
  * Link and Glue functions
  */
-void bldc3_link_uh (bldc3_t *bldc3, drv_out_f_ft uh);
-void bldc3_link_ul (bldc3_t *bldc3, drv_out_f_ft ul);
-void bldc3_link_vh (bldc3_t *bldc3, drv_out_f_ft vh);
-void bldc3_link_vl (bldc3_t *bldc3, drv_out_f_ft vl);
-void bldc3_link_wh (bldc3_t *bldc3, drv_out_f_ft wh);
-void bldc3_link_wl (bldc3_t *bldc3, drv_out_f_ft wl);
-void bldc3_link_u_emf (bldc3_t *bldc3, drv_ain_i_ft u_emf);
-void bldc3_link_v_emf (bldc3_t *bldc3, drv_ain_i_ft v_emf);
-void bldc3_link_w_emf (bldc3_t *bldc3, drv_ain_i_ft w_emf);
-void bldc3_link_i_br (bldc3_t *bldc3, drv_ain_f_ft i_br);
-void bldc3_link_pid (bldc3_t *bldc3, pid_c_t *pid);
+void bldc_link_uh (bldc_t *bldc, drv_out_f_ft uh);
+void bldc_link_ul (bldc_t *bldc, drv_out_f_ft ul);
+void bldc_link_vh (bldc_t *bldc, drv_out_f_ft vh);
+void bldc_link_vl (bldc_t *bldc, drv_out_f_ft vl);
+void bldc_link_wh (bldc_t *bldc, drv_out_f_ft wh);
+void bldc_link_wl (bldc_t *bldc, drv_out_f_ft wl);
+void bldc_link_u_emf (bldc_t *bldc, drv_ain_i_ft u_emf);
+void bldc_link_v_emf (bldc_t *bldc, drv_ain_i_ft v_emf);
+void bldc_link_w_emf (bldc_t *bldc, drv_ain_i_ft w_emf);
+void bldc_link_i_br (bldc_t *bldc, drv_ain_f_ft i_br);
+void bldc_link_pid (bldc_t *bldc, pid_c_t *pid);
 
 /*
  * Set functions
  */
-drv_status_en bldc3_set_poles (bldc3_t *bldc3, int poles);
-drv_status_en bldc3_set_rpm (bldc3_t *bldc3, bldc3_rpm_t rpm);
-drv_status_en bldc3_set_i_br (bldc3_t *bldc3, float i_br);
-drv_status_en bldc3_set_dir (bldc3_t *bldc3, bldc3_dir_en dir);
+drv_status_en bldc_set_poles (bldc_t *bldc, int poles);
+drv_status_en bldc_set_rpm (bldc_t *bldc, bldc_rpm_t rpm);
+drv_status_en bldc_set_i_br (bldc_t *bldc, float i_br);
+drv_status_en bldc_set_dir (bldc_t *bldc, bldc_dir_en dir);
 
 /*
  * User Functions
  */
-void bldc3_startup (bldc3_t *bldc3);
-void bldc3_stop (bldc3_t *bldc3);
+void bldc_startup (bldc_t *bldc);
+void bldc_roll (bldc_t *bldc);
+void bldc_stop (bldc_t *bldc);
 
-drv_status_en bldc3_init (bldc3_t *bldc3);
-drv_status_en bldc3_control (bldc3_t *bldc3, uint8_t wait);
+drv_status_en bldc_init (bldc_t *bldc);
+drv_status_en bldc_control (bldc_t *bldc, uint8_t wait);
 
-drv_status_en bldc3_ioctl (bldc3_t *bldc3, ioctl_cmd_t cmd, ioctl_buf_t buf);
+drv_status_en bldc_ioctl (bldc_t *bldc, ioctl_cmd_t cmd, ioctl_buf_t buf);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif   //#ifndef __bldc3_emf_h__
+#endif   //#ifndef __bldc_emf_h__
---
+++ src/drv/bldc_emf.c
@@ -0,0 +1,227 @@
+/*!
+ * \file bldc_emf.c
+ * \brief
+ *    A target independent brushless DC motor with back-emf.
+ *
+ * This file is part of toolbox
+ *
+ * Copyright (C) 2014 Houtouridis Christos (http://www.houtouridis.net)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <drv/bldc_emf.h>
+
+inline void _set_output_off (bldc_t *bldc)
+{
+   bldc->io.uh (0);
+   bldc->io.ul (0);
+   bldc->io.vh (0);
+   bldc->io.vl (0);
+   bldc->io.wh (0);
+   bldc->io.wl (0);
+}
+
+inline void _set_output (bldc_t *bldc, bldc_state_en state, float speed)
+{
+   switch (state) {
+      default:
+      case BLDC_ST0:
+         bldc->io.uh (0); bldc->io.vl (0); bldc->io.wh (0); bldc->io.wl (0);
+         bldc->io.ul (100);      bldc->io.vh (100);
+         break;
+      case BLDC_ST1:
+         bldc->io.uh (0); bldc->io.vh (0); bldc->io.vl (0); bldc->io.wl (0);
+         bldc->io.ul (100);                              bldc->io.wh (100);
+         break;
+      case BLDC_ST2:
+         bldc->io.uh (0); bldc->io.ul (0); bldc->io.vh (0); bldc->io.wl (0);
+                                 bldc->io.vl (100);      bldc->io.wh (100);
+         break;
+      case BLDC_ST3:
+         bldc->io.ul (0); bldc->io.vh (0); bldc->io.wh (0); bldc->io.wl (0);
+         bldc->io.uh (100);      bldc->io.vl (100);
+         break;
+      case BLDC_ST4:
+         bldc->io.ul (0); bldc->io.vh (0); bldc->io.vl (0); bldc->io.wh (0);
+         bldc->io.uh (100);                              bldc->io.wl (100);
+         break;
+      case BLDC_ST5:
+         bldc->io.uh (0); bldc->io.ul (0); bldc->io.vl (0); bldc->io.wh (0);
+                                 bldc->io.vh (100);      bldc->io.wl (100);
+         break;
+   }
+
+}
+
+/*
+ *  ============= PUBLIC BLDC API =============
+ */
+
+/*
+ * Link and Glue functions
+ */
+inline void bldc_link_uh (bldc_t *bldc, drv_out_f_ft uh) {
+   bldc->io.uh = uh;
+}
+inline void bldc_link_ul (bldc_t *bldc, drv_out_f_ft ul) {
+   bldc->io.ul = ul;
+}
+inline void bldc_link_vh (bldc_t *bldc, drv_out_f_ft vh) {
+   bldc->io.vh = vh;
+}
+inline void bldc_link_vl (bldc_t *bldc, drv_out_f_ft vl) {
+   bldc->io.vl = vl;
+}
+inline void bldc_link_wh (bldc_t *bldc, drv_out_f_ft wh) {
+   bldc->io.wh = wh;
+}
+inline void bldc_link_wl (bldc_t *bldc, drv_out_f_ft wl) {
+   bldc->io.wl = wl;
+}
+inline void bldc_link_u_emf (bldc_t *bldc, drv_ain_i_ft u_emf) {
+   bldc->io.u_emf = u_emf;
+}
+inline void bldc_link_v_emf (bldc_t *bldc, drv_ain_i_ft v_emf) {
+   bldc->io.v_emf = v_emf;
+}
+void bldc_link_w_emf (bldc_t *bldc, drv_ain_i_ft w_emf) {
+   bldc->io.w_emf = w_emf;
+}
+void bldc_link_i_br (bldc_t *bldc, drv_ain_f_ft i_br) {
+   bldc->io.I_br = i_br;
+}
+void bldc_link_pid (bldc_t *bldc, pid_c_t *pid) {
+   bldc->io.pid = pid;
+}
+
+/*
+ * Set functions
+ */
+drv_status_en bldc_set_poles (bldc_t *bldc, int poles) {
+   if ( poles<BLDC_STATES )
+      return DRV_ERROR;
+   bldc->set.poles = poles;
+   return bldc->status = DRV_READY;
+}
+
+drv_status_en bldc_set_rpm (bldc_t *bldc, bldc_rpm_t rpm) {
+   if ( rpm < BLDC_MIN_RPM || rpm > BLDC_MAX_RPM(bldc->set.poles))
+      return DRV_ERROR;
+   bldc->set.Freq_r = rpm / 60;
+   return bldc->status = DRV_READY;
+}
+
+drv_status_en bldc_set_i_br (bldc_t *bldc, float i_br) {
+   if (i_br < 0)
+      return DRV_ERROR;
+   bldc->set.I_br = i_br;
+
+   return bldc->status = DRV_READY;
+}
+
+drv_status_en bldc_set_dir (bldc_t *bldc, bldc_dir_en dir) {
+   if (! (dir==BLDC_FWD || dir == BLDC_REV) )
+      return DRV_ERROR;
+   bldc->set.dir = dir;
+   return bldc->status = DRV_READY;
+}
+
+/*
+ * User Functions
+ */
+
+void bldc_startup (bldc_t *bldc)
+{
+   int i;// dl = BLDC_RPM2USEC (600, bldc->set.poles);
+
+   // Clear all outputs
+   _set_output_off (bldc);
+   jf_delay_ms (1000);
+
+   // charge low site capacitors
+   bldc->io.ul (100);   bldc->io.vl (100);   bldc->io.wl (100);
+   jf_delay_ms (500);
+   bldc->io.ul (0);     bldc->io.vl (0);     bldc->io.wl (0);
+
+   /*
+   // find rotor
+   for (i=0 ; i<bldc->set.poles*2 ; ++i) {
+      _set_output (bldc, i);
+      jf_delay_us (dl);
+   }
+   */
+   _set_output (bldc, BLDC_ST5, 0.1);
+   jf_delay_ms (200);
+   _set_output_off (bldc);
+
+}
+
+void bldc_roll (bldc_t *bldc)
+{
+   int i, dl = BLDC_RPM2USEC (600, bldc->set.poles);
+
+   // charge low site capacitors
+   bldc->io.ul (100);   bldc->io.vl (100);   bldc->io.wl (100);
+   jf_delay_ms (100);
+   bldc->io.ul (0);     bldc->io.vl (0);     bldc->io.wl (0);
+
+   for (i=0 ; i<bldc->set.poles ; ++i) {
+      _set_output (bldc, i%6, 2);
+      jf_delay_us (dl);
+   }
+}
+
+void bldc_stop (bldc_t *bldc);
+
+drv_status_en bldc_init (bldc_t *bldc)
+{
+   #define _bldc_assert(_x)   if (!(_x)) return bldc->status = DRV_ERROR;
+
+   drv_status_en st = jf_probe ();
+
+   if (st == DRV_NODEV || st == DRV_NOINIT) {
+      if ( jf_init (BLDC_JF_FREQ, BLDC_JF_ARVALUE) )
+         return bldc->status = DRV_ERROR;
+   }
+   else  // if  (st == DRV_NODEV || st == DRV_NOINIT)
+      return bldc->status = DRV_ERROR;
+
+   _bldc_assert (bldc->io.uh);
+   _bldc_assert (bldc->io.ul);
+   _bldc_assert (bldc->io.vh);
+   _bldc_assert (bldc->io.vl);
+   _bldc_assert (bldc->io.wh);
+   _bldc_assert (bldc->io.wl);
+   _bldc_assert (bldc->io.u_emf);
+   _bldc_assert (bldc->io.v_emf);
+   _bldc_assert (bldc->io.w_emf);
+   //_bldc_assert (bldc->io.pid);
+   //_bldc_assert (bldc->io.I_br);        // Optional feature
+
+   _bldc_assert ( bldc->set.poles >= BLDC_STATES );
+
+   /*
+    * We are here, so all its OK.
+    */
+   return bldc->status = DRV_READY;
+   #undef _bldc_assert
+}
+
+drv_status_en bldc_control (bldc_t *bldc, uint8_t wait);
+
+drv_status_en bldc_ioctl (bldc_t *bldc, ioctl_cmd_t cmd, ioctl_buf_t buf);
+
+
