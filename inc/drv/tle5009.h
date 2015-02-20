ced1c9f7f691986df8cd937c9e78169f0398281e
Christos Houtouridis <houtouridis.ch@gmail.com>
2015-02-20 15:28:21 +0200

    DEV/tle5009: New driver part 1

---
+++ inc/drv/tle5009.h
@@ -0,0 +1,41 @@
+/*
+ * \file tle5009.h
+ * \brief
+ *    A target independent TLE-5009 sensor driver
+ *
+ * This file is part of toolbox
+ *
+ * Copyright (C) 2014 Houtouridis Christos (http://www.houtouridis.net)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#ifndef __tle5009_h__
+#define __tle5009_h__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+
+
+
+#ifdef __cplusplus
+ }
+#endif
+
+#endif //#ifndef __tle5009_h__
