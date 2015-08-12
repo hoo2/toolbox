5178712b6b04ec5f7f3584e3227e4cfb3db083d5
Christos Houtouridis <houtouridis.ch@gmail.com>
2015-08-12 16:59:06 +0300

    DEV/DFT: A full working dft for educational perposes added

--- inc/dsp/dft.h
+++ inc/dsp/dft.h
@@ -1,11 +1,11 @@
 /*
  * \file dft.h
  * \brief
- *    A target independent Discrete Fourier Transform functionality
+ *    A target independent Discrete Fourier Transform implementation
  *
  * This file is part of toolbox
  *
- * Copyright (C) 2014 Houtouridis Christos (http://www.houtouridis.net)
+ * Copyright (C) 2015 Houtouridis Christos (http://www.houtouridis.net)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -28,21 +28,89 @@
 extern "C" {
 #endif
 
-#include <math.h>
-#include <stdint.h>
-#include <tbx_types.h>
-#include <dsp/quick_trig.h>
-
-#ifndef M_PI
-#define M_PI     3.14159265358979323846
-#endif
+#include <dsp/dsp.h>
 
 /*
  * ================== Public API ====================
  */
-void rdft_i (int *re, int *im, int *x, int n);
-void rdft_f (float *re, float *im, float *x, int n);
-void rdft_d (double *re, double *im, double *x, int n);
+// Forward DFT
+#if __STDC_VERSION__ >= 201112L
+
+#ifndef fft
+/*!
+ * A pseudo type-polymorphism mechanism using _Generic macro
+ * to simulate:
+ *
+ * template<typename T> void dft (T *x, T *X, uint32_t n);
+ *
+ * \note We still have to implement all the functions
+ *
+ * \brief
+ *    Calculate the complex DFT using a not in-place DFT matrix algorithm.
+ *
+ * \param   x     Pointer to size n time domain array
+ * \param   X     Pointer to size n frequency domain complex array
+ * \param   n     Number of points
+ * \return        None
+ */
+#define dft(x, X, n)       _Generic((x),  \
+       complex_d_t*: dft_c,               \
+       complex_f_t*: dft_cf,              \
+       complex_i_t*: dft_cf,              \
+            double*: dft_r,               \
+             float*: dft_rf,              \
+               int*: dft_rf,              \
+            default: dft_r)(x, X, n)
+#endif   // #ifndef dft
+#endif   // #if __STDC_VERSION__ >= 201112L
+
+void dft_c (complex_d_t *x, complex_d_t *X, uint32_t n) __optimize__ ;
+void dft_cf (complex_f_t *x, complex_f_t *X, uint32_t n) __optimize__ ;
+void dft_r (double *x, complex_d_t *X, uint32_t n) __optimize__ ;
+void dft_rf (float *x, complex_f_t *X, uint32_t n) __optimize__ ;
+
+
+// Inverse DFT
+#if __STDC_VERSION__ >= 201112L
+
+#ifndef idft
+/*!
+ * A pseudo type-polymorphism mechanism using _Generic macro
+ * to simulate:
+ *
+ * template<typename T> void idft (T *X, T *x, uint32_t n);
+ *
+ * \note We still have to implement all the functions
+ *
+ * \brief
+ *    Calculate the double precision inverse DFT using a
+ *    not in-place DFT matrix algorithm.
+ *
+ * \note
+ *    Due to symmetrical property of real DFT the algorithm doesn't use the
+ *    negative frequencies of frequency spectra. So in the case of real inverse DFT
+ *    the frequency array can have size n/2+1.
+ *
+ * \param   X     Pointer to size n frequency domain array
+ * \param   x     Pointer to size n time domain array
+ * \param   n     Number of points
+ * \return        None
+ */
+#define idft(X, x, n)      _Generic((x),  \
+       complex_d_t*: idft_c,              \
+       complex_f_t*: idft_cf,             \
+       complex_i_t*: idft_cf,             \
+            double*: idft_r,              \
+             float*: idft_rf,             \
+               int*: idft_rf,             \
+            default: idft_r)(X, x, n)
+#endif   // #ifndef idft
+#endif   // #if __STDC_VERSION__ >= 201112L
+
+void idft_c (complex_d_t *X, complex_d_t *x, uint32_t n) __optimize__ ;
+void idft_cf (complex_f_t *X, complex_f_t *x, uint32_t n) __optimize__ ;
+void idft_r (complex_d_t *X, double *x, uint32_t n) __optimize__ ;
+void idft_rf (complex_f_t *X, float *x, uint32_t n) __optimize__ ;
 
 #ifdef __cplusplus
 }
--- src/dsp/dft.c
+++ src/dsp/dft.c
@@ -1,11 +1,11 @@
 /*
  * \file dft.c
  * \brief
- *    A target independent Discrete Fourier Transform functionality
+ *    A target independent Discrete Fourier Transform implementation
  *
  * This file is part of toolbox
  *
- * Copyright (C) 2014 Houtouridis Christos (http://www.houtouridis.net)
+ * Copyright (C) 2015 Houtouridis Christos (http://www.houtouridis.net)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
@@ -23,35 +23,268 @@
  */
 #include <dsp/dft.h>
 
-void rdft_i (int *re, int *im, int *x, int n)
+/*!
+ * \brief
+ *    Calculate the double precision complex DFT using a
+ *    not in-place DFT matrix algorithm.
+ *
+ * \param   x     Pointer to size n time domain complex array
+ * \param   X     Pointer to size n frequency domain complex array
+ * \param   n     Number of points
+ * \return        None
+ */
+void dft_c (complex_d_t *x, complex_d_t *X, uint32_t n)
 {
-   int N=n, k, K=n/2+1;
-   float wn, _2pik_N, _2pi_N = 2*M_PI/N;
+   uint32_t i, j;
+   double th, _2pi_n, _2pii_n;
+   complex_d_t w;
 
-   for (k=0 ; k<K ; ++k) {
-      _2pik_N = _2pi_N * k;   // calculate omega
-      re[k] = im[k] = 0;      // empty acc
-      for (n=0 ; n<N ; ++n) {
-         wn = _2pik_N * n;    // calculate Omega * n
-         re[k] += x[n] * qcos (wn);
-         im[k] -= x[n] * qsin (wn);
+   _2pi_n = 2*M_PI/n;
+   for (i=0 ; i<n ; ++i) {
+      _2pii_n = _2pi_n * i;   // calculate omega
+      X[i] = 0;               // empty acc
+      for (j=0 ; j<n ; ++j) {
+         th = _2pii_n * j;    // calculate Omega * j
+         w = cos(th) - I*sin(th);
+         X[i] += w * x[j];
       }
    }
 }
 
-void rdft_f (float *re, float *im, float *x, int n)
+/*!
+ * \brief
+ *    Calculate the single precision complex DFT using a
+ *    not in-place DFT matrix algorithm.
+ *
+ * \param   x     Pointer to size n time domain complex array
+ * \param   X     Pointer to size n frequency domain complex array
+ * \param   n     Number of points
+ * \return        None
+ */
+void dft_cf (complex_f_t *x, complex_f_t *X, uint32_t n)
 {
-   int N=n, k, K=n/2+1;
-   float wn, _2pik_N, _2pi_N = 2*M_PI/N;
+   uint32_t i, j;
+   float th, _2pi_n, _2pii_n;
+   complex_f_t w;
 
-   for (k=0 ; k<K ; ++k) {
-      _2pik_N = _2pi_N * k;   // calculate omega
-      re[k] = im[k] = 0;      // empty acc
-      for (n=0 ; n<N ; ++n) {
-         wn = _2pik_N * n;    // calculate Omega * n
-         re[k] += x[n] * qcos (wn);
-         im[k] -= x[n] * qsin (wn);
+   _2pi_n = 2*M_PI/n;
+   for (i=0 ; i<n ; ++i) {
+      _2pii_n = _2pi_n * i;   // calculate omega
+      X[i] = 0;               // empty acc
+      for (j=0 ; j<n ; ++j) {
+         th = _2pii_n * j;    // calculate Omega * j
+         w = cos(th) - I*sin(th);
+         X[i] += w * x[j];
       }
    }
 }
-void rdft_d (double *re, double *im, double *x, int n);
+
+/*!
+ * \brief
+ *    Calculate the double precision DFT for real signals using a
+ *    not in-place DFT matrix algorithm.
+ *
+ * \param   x     Pointer to size n time domain real array
+ * \param   X     Pointer to size n frequency domain complex array
+ * \param   n     Number of points
+ * \return        None
+ */
+void dft_r (double *x, complex_d_t *X, uint32_t n)
+{
+   uint32_t i, j, n_2;
+   double th, _2pi_n, _2pii_n;
+   complex_d_t w;
+
+   _2pi_n = 2*M_PI/n;
+   n_2 = n>>1;
+   for (i=0 ; i<n ; ++i) {
+      _2pii_n = _2pi_n * i;   // calculate omega
+      X[i] = 0;               // empty acc
+      for (j=0 ; j<=n_2 ; ++j) {
+         th = _2pii_n * j;    // calculate Omega * j
+         w = cos(th) - I*sin(th);
+         X[i] += w * x[j];
+      }
+   }
+   /*
+    *  For real DFT the Frequency spectra is symmetrical.
+    *  Hence we can copy the negative frequencies from positives
+    */
+   for (i=n_2+1 ; i<n ; ++i)
+      X[i] = conj (X[n-i]);
+}
+
+/*!
+ * \brief
+ *    Calculate the single precision DFT for real signals using a
+ *    not in-place DFT matrix algorithm.
+ *
+ * \param   x     Pointer to size n time domain real array
+ * \param   X     Pointer to size n frequency domain complex array
+ * \param   n     Number of points
+ * \return        None
+ */
+void dft_rf (float *x, complex_f_t *X, uint32_t n)
+{
+   uint32_t i, j, n_2;
+   float th, _2pi_n, _2pii_n;
+   complex_f_t w;
+
+   _2pi_n = 2*M_PI/n;
+   n_2 = n>>1;
+   for (i=0 ; i<n ; ++i) {
+      _2pii_n = _2pi_n * i;   // calculate omega
+      X[i] = 0;               // empty acc
+      for (j=0 ; j<=n_2 ; ++j) {
+         th = _2pii_n * j;    // calculate Omega * j
+         w = cos(th) - I*sin(th);
+         X[i] += w * x[j];
+      }
+   }
+   /*
+    *  For real DFT the Frequency spectra is symmetrical.
+    *  Hence we can copy the negative frequencies from positives
+    */
+   for (i=n_2+1 ; i<n ; ++i)
+      X[i] = conjf (X[n-i]);
+}
+
+/*!
+ * \brief
+ *    Calculate the double precision inverse DFT using a
+ *    not in-place DFT matrix algorithm.
+ *
+ * \param   X     Pointer to size n frequency domain complex array
+ * \param   x     Pointer to size n time domain complex array
+ * \param   n     Number of points
+ * \return        None
+ */
+void idft_c (complex_d_t *X, complex_d_t *x, uint32_t n)
+{
+   uint32_t i, j;
+   double th, _2pi_n, _2pii_n, _1_n;
+   complex_d_t w;
+
+   _2pi_n = 2*M_PI/n;
+   _1_n = 1./n;
+   for (i=0 ; i<n ; ++i) {
+      _2pii_n = _2pi_n * i;   // calculate omega
+      x[i] = 0;               // empty acc
+      for (j=0 ; j<n ; ++j) {
+         th = _2pii_n * j;    // calculate Omega * j
+         w = cos(th) + I*sin(th);
+         x[i] += w * X[j];
+      }
+      x[i] *= _1_n;           // scale output
+   }
+}
+
+/*!
+ * \brief
+ *    Calculate the single precision inverse DFT using a
+ *    not in-place DFT matrix algorithm.
+ *
+ * \param   X     Pointer to size n frequency domain complex array
+ * \param   x     Pointer to size n time domain complex array
+ * \param   n     Number of points
+ * \return        None
+ */
+void idft_cf (complex_f_t *X, complex_f_t *x, uint32_t n)
+{
+   uint32_t i, j;
+   float th, _2pi_n, _2pii_n, _1_n;
+   complex_f_t w;
+
+   _2pi_n = 2*M_PI/n;
+   _1_n = 1./n;
+   for (i=0 ; i<n ; ++i) {
+      _2pii_n = _2pi_n * i;   // calculate omega
+      x[i] = 0;               // empty acc
+      for (j=0 ; j<n ; ++j) {
+         th = _2pii_n * j;    // calculate Omega * j
+         w = cos(th) + I*sin(th);
+         x[i] += w * X[j];
+      }
+      x[i] *= _1_n;           // scale output
+   }
+}
+
+/*!
+ * brief
+ *    inverse dft contribution loop body
+ */
+#define _idft_r_loop(_j, _div)   \
+{                                \
+   th = _2pii_n * _j;            \
+   w = cos(th) + I*sin(th);      \
+   x[i] += w * (X[_j] * _div);   \
+}
+
+/*!
+ * \brief
+ *    Calculate the double precision inverse DFT for real signals
+ *    using a not in-place DFT matrix algorithm.
+ * \note
+ *    Due to symmetrical property of real DFT the algorithm doesn't use the
+ *    negative frequencies of frequency spectra. So the frequency array can
+ *    also be smaller.
+ *
+ * \param   X     Pointer to size n or n/2+1 frequency domain complex array
+ * \param   x     Pointer to size n time domain real array
+ * \param   n     Number of points
+ * \return        None
+ */
+void idft_r (complex_d_t *X, double *x, uint32_t n)
+{
+   uint32_t i, j, n_2;
+   double th, _2pi_n, _2pii_n, _1_n, _2_n;
+   complex_d_t w;
+
+   _2pi_n = 2*M_PI/n;
+   _1_n = 1./n;
+   _2_n = 2./n;
+   n_2 = n>>1;
+   for (i=0 ; i<n ; ++i) {
+      _2pii_n = _2pi_n * i;   // calculate omega
+      x[i] = 0;               // empty acc
+      _idft_r_loop(0, _1_n);
+      for (j=1 ; j<n_2 ; ++j)
+         _idft_r_loop(j, _2_n);
+      _idft_r_loop(n_2, _1_n);
+   }
+}
+
+/*!
+ * \brief
+ *    Calculate the single precision inverse DFT for real signals
+ *    using a not in-place DFT matrix algorithm.
+ * \note
+ *    Due to symmetrical property of real DFT the algorithm doesn't use the
+ *    negative frequencies of frequency spectra. So the frequency array can
+ *    also be smaller.
+ *
+ * \param   X     Pointer to size n or n/2+1 frequency domain complex array
+ * \param   x     Pointer to size n time domain real array
+ * \param   n     Number of points
+ * \return        None
+ */
+void idft_rf (complex_f_t *X, float *x, uint32_t n)
+{
+   uint32_t i, j, n_2;
+   float th, _2pi_n, _2pii_n, _1_n, _2_n;
+   complex_f_t w;
+
+   _2pi_n = 2*M_PI/n;
+   _1_n = 1./n;
+   _2_n = 2./n;
+   n_2 = n>>1;
+   for (i=0 ; i<n ; ++i) {
+      _2pii_n = _2pi_n * i;   // calculate omega
+      x[i] = 0;               // empty acc
+      _idft_r_loop(0, _1_n);
+      for (j=1 ; j<n_2 ; ++j)
+         _idft_r_loop(j, _2_n);
+      _idft_r_loop(n_2, _1_n);
+   }
+}
+
