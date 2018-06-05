c097463d975a317e6a5e4f63f2e97f2f169d8ed7
Christos Houtouridis <houtouridis.ch@gmail.com>
2018-06-05 15:39:23 +0300

    DEV: A new character map added

---
+++ inc/drv/w1602b_cs.h
@@ -0,0 +1,88 @@
+/*
+ * \file   w1602b_cs.h
+ * \brief  Winstar W1602B character set defines
+ *
+ * This file is part of toolbox
+ *
+ * Copyright (C) 2015 Houtouridis Christos (http://www.houtouridis.net)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef __w1602b_cs_h__
+#define __w1602b_cs_h__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define  _Deg_     "\xB2"      // Degrese
+#define  _pow2_    "\x1E"      // ^2
+#define  _pow3_    "\x1F"      // ^3
+#define  _A_       "A"
+#define  _B_       "B"
+#define  _G_       "\xD4"      // Gamma
+#define  _D_       "\x7F"      // Delta
+#define  _E_       "E"
+#define  _Z_       "Z"
+#define  _H_       "H"
+#define  _TH_      "\xD6"      // Theta
+#define  _I_       "I"
+#define  _K_       "K"
+#define  _L_       "\xD7"      // Lambda
+#define  _M_       "M"
+#define  _N_       "N"
+#define  _J_       "\xD8"      // Ksi
+#define  _O_       "O"
+#define  _P_       "\xD9"      // Pi
+#define  _R_       "P"
+#define  _S_       "\xDA"      // Sigma
+#define  _T_       "T"
+#define  _U_       "Y"
+#define  _F_       "\xDC"      // Phi
+#define  _X_       "X"
+#define  _Y_       "\xDD"      // Psi
+#define  _W_       "\xDE"      // Omega
+
+#define  _a_       "\xDF"      // alpha
+#define  _b_       "\xE0"      // beta
+#define  _g_       "\xE1"      // gamma
+#define  _d_       "\xE2"      // delta
+#define  _e_       "\xE3"      // epsilon
+#define  _z_       "\xE4"      // zeta
+#define  _h_       "\xE5"      // heta
+#define  _th_      "\xE6"      // theta
+#define  _i_       "\xE7"      // iota
+#define  _k_       "\xE8"      // kapa
+#define  _l_       "\xE9"      // lambda
+#define  _m_       "\xEA"      // mi
+#define  _n_       "\xEB"      // ni
+#define  _j_       "\xEC"      // ksi
+#define  _o_       "o"         // omikron
+#define  _p_       "\xED"      // pi
+#define  _r_       "\xEE"      // ro
+#define  _s_       "\xEF"      // sigma
+#define  _t_       "\xF0"      // taph
+#define  _u_       "\xF1"      // upsilon
+#define  _f_       "\xDC"      // phi
+#define  _x_       "\xF2"      // hi
+#define  _y_       "\xF3"      // psi
+#define  _w_       "\xF4"      // omega
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif   // #ifndef __w1602b_cs_h__
